fc03eb13a609f51b093c56eb66b032c0c2d9c780
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-19 21:33:25 -0300

    create /authenticate route

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express from 'express'
+import { router } from '../routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+app.get('/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query
+
+  res.json(code)
+})
+
+app.listen(3333, () => console.log('service running on port 3333'))
