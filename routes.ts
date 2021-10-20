d83d3d31324d78284a2ce73129bf6a41a0cd79d8
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-20 19:38:02 -0300

    routes moved to right place

--- routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { AuthenticateUserController } from "./src/controllers/AuthenticateUserController";
-
-const router = Router()
-
-router.post('/authenticate', new AuthenticateUserController().handle)
-
-export { router }
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
-import { router } from '../routes'
+import { router } from './routes'
 
 const app = express()
 
